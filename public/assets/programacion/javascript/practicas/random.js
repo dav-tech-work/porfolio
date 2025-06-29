67fbf43838eb6545e5e88e73a5983e0dbfbcfb88
Daniel Arribas Velázquez <danielarribasvelazquez@gmail.com>
2025-06-29 22:24:32 +0200

    Update

--- public/assets/programacion/javascript/practicas/random.js
+++
@@ -1,9 +0,0 @@
-function random10 (min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-
-    console.log(Math.floor(Math.random() * (max-min +1) + min));
-}
-random10(1,10)
-random10(1,10)
-
